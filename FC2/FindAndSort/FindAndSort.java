b9325e236408ec9f301e8990dd4c942a8cad9235
Vustey <vutv579@gmail.com>
2023-06-07 21:40:05 +0700

    update FindAndSort

---
+++ FC2/FindAndSort/FindAndSort.java
@@ -0,0 +1,66 @@
+package FC2.FindAndSort;
+
+import java.util.*;
+
+public class FindAndSort {
+    int a[];
+    int b[];
+    Scanner scanner = new Scanner(System.in);
+
+    void random(int n) {
+        Random random = new Random();
+        a = new int[n];
+        b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = random.nextInt(100);
+            b[i] = random.nextInt(100);
+        }
+
+    }
+
+    void copyArray() {
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+    }
+
+    void output() {
+        System.out.print("a[" + a.length + "] = ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.print("b[" + b.length + "] = ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+
+    void selectSort() {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int newIndex = i;
+            for(int j = i + 1; j < n; j++){
+                if(a[j] < a[newIndex]){
+                    newIndex = j;
+                }
+            int temp = a[i];
+                a[i] = a[newIndex];
+                a[newIndex] = temp;
+            }
+        }
+    }
+
+    void bubbleSort() {
+
+    }
+
+    public static void main(String[] args) {
+        FindAndSort findAndSort = new FindAndSort();
+        findAndSort.random(5);
+        findAndSort.selectSort();
+        findAndSort.copyArray();
+        findAndSort.output();
+
+    }
+}
